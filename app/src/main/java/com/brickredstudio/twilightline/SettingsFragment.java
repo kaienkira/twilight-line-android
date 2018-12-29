b9a6199514dbd9a271ba2dc573fdf902d043a127
kaien <kaienkira@gmail.com>
2018-12-30 00:13:19 +0800

    work sync

--- app/src/main/java/com/brickredstudio/twilightline/SettingsFragment.java
+++ app/src/main/java/com/brickredstudio/twilightline/SettingsFragment.java
@@ -1,6 +1,8 @@
 package com.brickredstudio.twilightline;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsFragment extends PreferenceFragmentCompat
@@ -9,5 +11,13 @@ public class SettingsFragment extends PreferenceFragmentCompat
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey)
     {
         setPreferencesFromResource(R.xml.settings, rootKey);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
+    {
+		super.onCreateOptionsMenu(menu, inflater);
+		inflater.inflate(R.menu.main, menu);
     }
 }
