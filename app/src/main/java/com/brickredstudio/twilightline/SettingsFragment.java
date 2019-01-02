53a8d419da84f5130505d3bac8158279b4cf4443
kaien <kaienkira@gmail.com>
2019-01-02 23:49:10 +0800

    work sync

--- app/src/main/java/com/brickredstudio/twilightline/SettingsFragment.java
+++ app/src/main/java/com/brickredstudio/twilightline/SettingsFragment.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MenuInflater;
+import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.Toast;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsFragment extends PreferenceFragmentCompat
 {
+    private SettingsFragment thiz = null;
     private Switch switchProxyButton = null;
 
     @Override
@@ -16,6 +19,7 @@ public class SettingsFragment extends PreferenceFragmentCompat
     {
         setPreferencesFromResource(R.xml.settings, rootKey);
         setHasOptionsMenu(true);
+        this.thiz = this;
     }
 
     @Override
@@ -28,6 +32,19 @@ public class SettingsFragment extends PreferenceFragmentCompat
             MenuItem item = menu.findItem(R.id.switch_proxy);
             this.switchProxyButton =
                 item.getActionView().findViewById(R.id.switch_proxy_button);
+            this.switchProxyButton.setOnCheckedChangeListener(
+                new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        thiz.onSwitchProxyButtonCheckedChanged(isChecked);
+                    }
+                });
         }
+    }
+
+    public void onSwitchProxyButtonCheckedChanged(boolean isChecked)
+    {
+        Toast.makeText(getActivity(), String.format("%b", isChecked),
+            Toast.LENGTH_SHORT).show();
     }
 }
