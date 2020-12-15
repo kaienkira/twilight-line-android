a3d5645cd8e437f22503cab164e4b6554e35ec15
kaien <kaienkira@gmail.com>
2020-12-15 18:45:57 +0800

    work sync

--- app/src/main/java/com/brickredstudio/twilightline/TwilightLineVpnService.java
+++ app/src/main/java/com/brickredstudio/twilightline/TwilightLineVpnService.java
@@ -1,5 +1,6 @@
 package com.brickredstudio.twilightline;
 
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.net.VpnService;
 import androidx.core.app.NotificationCompat;
@@ -9,12 +10,18 @@ public class TwilightLineVpnService extends VpnService
     @Override
     public int onStartCommand(Intent intent, int flags, int startId)
     {
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+            App.getContext(), 0,
+            new Intent(App.getContext(), MainActivity.class),
+            PendingIntent.FLAG_UPDATE_CURRENT);
+
         startForeground(MainActivity.NOTIFICATION_ID,
             new NotificationCompat.Builder(
                 this, MainActivity.NOTIFICATION_CHANNEL_ID)
+            .setSmallIcon(R.drawable.app_icon)
             .setContentTitle("Twilight Line")
             .setContentText("Twilight Line Proxy Started")
-            .setSmallIcon(R.drawable.app_icon)
+            .setContentIntent(pendingIntent)
             .build());
 
         return START_NOT_STICKY;
