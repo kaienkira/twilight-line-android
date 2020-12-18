97e2e97f5f4cab3c85d18c2c3a5e217894960219
kaien <kaienkira@gmail.com>
2020-12-18 10:50:32 +0800

    work sync

--- app/src/main/java/com/brickredstudio/twilightline/AppUtil.java
+++ app/src/main/java/com/brickredstudio/twilightline/AppUtil.java
@@ -3,7 +3,10 @@ package com.brickredstudio.twilightline;
 import android.annotation.SuppressLint;
 import android.app.Application;
 import android.os.Build;
+import android.util.Log;
 import java.lang.reflect.Method;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 
 public final class AppUtil
 {
@@ -20,8 +23,56 @@ public final class AppUtil
                 return (String)methodRef.invoke(null);
 
             } catch (Exception e) {
+                Log.e(App.TAG, String.format(
+                    "get process name failed: %s", e.toString()));
                 return "";
             }
         }
+    }
+    public static boolean copyAsset(
+        String assetName, String destFileName)
+    {
+        return copyAsset(assetName, destFileName, 1024);
+    }
+
+    public static boolean copyAsset(
+        String assetName, String destFileName, int buffSize)
+    {
+        InputStream input = null;
+        FileOutputStream output = null;
+        try {
+            input = App.getContext().getAssets().open(assetName);
+            output = new FileOutputStream(destFileName);
+
+            final byte data[] = new byte[buffSize];
+            for (;;) {
+                int count = input.read(data, 0, buffSize);
+                if (count < 0) {
+                    break;
+                }
+                output.write(data, 0, count);
+            }
+
+        } catch (Exception e) {
+            Log.e(App.TAG, String.format(
+                "copy asset failed: %s", e.toString()));
+            return false;
+
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (Exception e) {
+                }
+            }
+            if (output != null) {
+                try {
+                    output.close();
+                } catch (Exception e) {
+                }
+            }
+        }
+
+        return true;
     }
 }
